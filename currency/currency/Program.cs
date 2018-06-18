b6187da0808ca77eed6c572eca482f952b35ff56
hinaa1 <40368159+hinaa1@users.noreply.github.com>
2018-06-19 00:55:42 +0500

    code

---
+++ currency/currency/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Rupees");
+            double r = double.Parse(Console.ReadLine());
+
+            double dollar = 122, canada = 90.23, pound = 158.03, dinar = 394.27, darham = 32.49, euro = 138.53, riyal = 31.53, yuan = 18.53, aus = 88.57;
+            double result = 0;
+            Console.WriteLine("1 : Dollar");
+            Console.WriteLine("2 : Canada");
+            Console.WriteLine("3 : Pound");
+            Console.WriteLine("4 : Dinar");
+            Console.WriteLine("5 : Darham");
+            Console.WriteLine("6 : Euro");
+            Console.WriteLine("7 : Riyal");
+            Console.WriteLine("8 : Australian");
+            Console.WriteLine("9 : Yuan ");
+            string sel = Console.ReadLine();
+            string flag = "";
+            if (sel == "1")
+            {
+                flag = "Dollar";
+                result = r / dollar;
+            }
+
+            else if (sel == "2")
+            {
+                flag = "Canada";
+                result = r / canada;
+            }
+
+            else if (sel == "3")
+            {
+                flag = "Pound";
+                result = r / pound;
+            }
+
+            else if (sel == "4")
+            {
+                flag = "Dinar";
+                result = r / dinar;
+            }
+            else if (sel == "5")
+            {
+                flag = "Darham";
+                result = r / darham;
+            }
+            else if (sel == "6")
+            {
+                flag = "Euro";
+                result = r / euro;
+            }
+            else if (sel == "7")
+            {
+                flag = "Riyal";
+                result = r / riyal;
+            }
+            else if (sel == "8")
+            {
+                flag = "Australian";
+                result = r / aus;
+            }
+            else if (sel == "9")
+            {
+                flag = "yuan";
+                result = r / yuan;
+            }
+
+            else
+            {
+                Console.WriteLine("incorrect Input");
+            }
+            Console.WriteLine(flag + " = " + result);
+            string h = Console.ReadLine();
+
+        }
+    }
+}
